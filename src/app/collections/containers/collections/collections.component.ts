3c07f93bdb314d199d23830c1047ff7137692d8f
nightcodelearning <nightcodelearning@gmail.com>
2018-08-14 20:49:52 -0500

    collections componets part1

--- src/app/collections/containers/collections/collections.component.ts
+++ src/app/collections/containers/collections/collections.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { CollectionsService } from '../../services/collections.service';
+import { Collection } from '../../models/collection';
 
 @Component({
   selector: 'app-collections',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsComponent implements OnInit {
 
-  constructor() { }
+  collectionList: Collection[];
+  isEnabledCollectionCard: boolean
 
-  ngOnInit() {
+  constructor(private collectionService: CollectionsService, private authFire: AngularFireAuth, private rdb: AngularFireDatabase) {
+    this.collectionList = null;
+    this.isEnabledCollectionCard = false;
   }
 
+  ngOnInit() {
+    this.authFire.authState
+      .subscribe(
+        user => {
+          this.rdb.list('collections/' + user.uid).snapshotChanges()
+          .subscribe(item => {
+            this.collectionList = [];
+            item.forEach(element => {
+              let x = element.payload.toJSON();
+              x["$key"] = element.key;
+              this.collectionList.push(x as Collection);
+            });
+          });
+        }
+      )
+  }
+
+  showCollectionCard() {
+    this.isEnabledCollectionCard = !this.isEnabledCollectionCard;
+  }
+
+  enterCollection(collection: any){
+    console.log(collection);
+  }
+
+  removeCollection($key: string) {
+    this.collectionService.removeCollection($key);
+  }
 }
