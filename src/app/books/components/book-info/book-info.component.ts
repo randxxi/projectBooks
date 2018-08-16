1b33748c14a477463ffcd8de7a8e5fcb984b8d5e
RafelN <Rafael.Nino@bizagi.com>
2018-08-16 14:32:24 -0500

    ajuste add a collections

--- src/app/books/components/book-info/book-info.component.ts
+++ src/app/books/components/book-info/book-info.component.ts
@@ -5,11 +5,25 @@ import { Collection, ICollection } from '../../../collections/models/collection'
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { trigger, state, style, transition, animate, animation } from "@angular/animations";
 
 @Component({
   selector: 'app-book-info',
   templateUrl: './book-info.component.html',
-  styleUrls: ['./book-info.component.styl']
+  styleUrls: ['./book-info.component.css'],
+  animations: [
+    trigger('popupAnimation', [
+      state('closed', style({
+        height: '0px',
+        display: 'none'
+      })),
+      state('opened', style({
+        height: '200px',
+      })),
+      transition('closed => opened', animate('90ms')),
+      transition('opened => closed', animate('90ms'))
+    ])
+  ]
 })
 export class BookInfoComponent implements OnInit {
 
@@ -18,11 +32,14 @@ export class BookInfoComponent implements OnInit {
 
   @Input() book: any;
   @Output() pushFavorite = new EventEmitter<any>();
-  
+  state:string;
+  popupState:string;
 
   constructor(private collectionService:CollectionService,private _bookService: BooksListService, private authFire: AngularFireAuth) {
     this.listCollections = null;
     this.collectionSelected = null;
+    this.state = 'open';
+    this.popupState = 'closed';
   }
 
   ngOnInit() {
@@ -42,7 +59,15 @@ export class BookInfoComponent implements OnInit {
   }
 
    addToCollection(event:any){
+
     this.collectionService.addBook(event.target.value,this.book);
+  }
+
+  onNavImageClick(){
+    if(this.popupState == 'closed')
+      this.popupState = 'opened'
+    else
+      this.popupState = 'closed'
   }
   
 }
